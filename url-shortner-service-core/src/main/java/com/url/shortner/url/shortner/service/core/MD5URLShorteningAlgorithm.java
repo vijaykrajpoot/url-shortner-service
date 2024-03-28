ae5e7724bb6f88783964af5a6d54f5f6316009ff
VIJAY KUMAR <vijaykumar@VIJAYs-MacBook-Pro.local>
2024-03-28 01:26:39 -0700

    Logs

--- url-shortner-service-core/src/main/java/com/url/shortner/url/shortner/service/core/MD5URLShorteningAlgorithm.java
+++ url-shortner-service-core/src/main/java/com/url/shortner/url/shortner/service/core/MD5URLShorteningAlgorithm.java
@@ -1,13 +1,17 @@
 package com.url.shortner.url.shortner.service.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
 @Service
 public class MD5URLShorteningAlgorithm implements URLShorteningAlgorithm {
+    private static final Logger logger = LoggerFactory.getLogger(MD5URLShorteningAlgorithm.class);
     private final MessageDigest messageDigest;
 
     public MD5URLShorteningAlgorithm() {
@@ -20,15 +24,24 @@ public class MD5URLShorteningAlgorithm implements URLShorteningAlgorithm {
 
     @Override
     public String shorten(String input) {
-        if (input != null) {
-            input = input + System.nanoTime();
+        if (input == null) {
+            throw new IllegalArgumentException("Inout can't be null");
         }
+        StringBuilder binaryStr = new StringBuilder();
+        // Add nano second to ensure has is always unique
+        input = input + System.nanoTime();
         byte[] byteArr = messageDigest.digest(input.getBytes());
+        logger.debug("byteArr: {}", Arrays.toString(byteArr));
+        // Convert byte array to bigInt by passing signum as 1 to eusure +ve big Integer
         BigInteger bigInteger = new BigInteger(1, byteArr);
+        logger.debug("bigInteger :{}", bigInteger);
         String bString = bigInteger.toString(2);
-        StringBuilder binaryStr = new StringBuilder(bString);
+        logger.debug("Binary String from BingInteger:{} With length:{}", bString, bString.length());
+        // append bString
+        binaryStr.append(bString);
+        // in case of length is less than 128 then pad the string with leading zero
         while (binaryStr.length() < 128) {
-            binaryStr.insert(0, "0"); // Pad with leading zeros if necessary
+            binaryStr.insert(0, "0");
         }
         return binaryStr.toString();
     }
